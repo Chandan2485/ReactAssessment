508efc599077d79e4854a2242daeeb2a409bcc0e
Chandan2485 <chandan96kar@gmail.com>
2022-10-21 23:06:27 +0530

    first commit

---
+++ Calculator/src/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+// import './index.css';
+// import {App} from './App';
+import { ColorRondam } from './Component/ColorRondam/ColorRondam';
+// import { Colorchange } from './Component/Colorchange/Colorchange';
+// import { Todo }from './Component/Todo/Todo';
+
+import reportWebVitals from './reportWebVitals';
+// import {Colortoggle} from './Component/ColorToggle/Colortoggle'
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    {/* <App /> */}
+    {/* <Colorchange/> */}
+    {/* <Todo /> */}
+    <ColorRondam/>
+    {/* <Colortoggle/> */}
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
