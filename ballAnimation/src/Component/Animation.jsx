7996b13d67b3483119f78388cb028a84a3760510
Chandan2485 <chandan96kar@gmail.com>
2022-11-01 03:36:59 +0530

    amimation

--- ballAnimation/src/Component/Animation.jsx
+++ ballAnimation/src/Component/Animation.jsx
@@ -1,12 +1,45 @@
+import './Animation.css'
 import React,{useState} from "react";
+// import { useEffect } from 'react';
+
+let index=0
 
 export function Animation (){
 
-    const [classname,setClassName]=useState()
+    const [classnamearr,setClassNameArr]=useState(['TopLeft','TopRight','BottomRight','BottomLeft'])
+    const [classname,setClassName]=useState('TopLeft')
+    // const [index,setIndex]=useState(2)
+
+
+
+    setInterval(() => {
+        setClassName(classnamearr[index])
+        if(index===3){
+                    index=0
+                }else{
+                    index++
+                }
+    }, 2000);
+
+
+
+
+
+    // useEffect(()=>{
+    //     console.log(index)
+    //     if(index===3){
+    //         setIndex(0)
+    //     }else{
+    //         setIndex((prev) => prev + 1)
+    //     }
+    // },[classname])
+
+
+
     return(
         <div className="mainDiv">
-        <div classname="Box TopLeft TopRight BottomRight BottomLeft">
-
+        <div className={`Box ${classname}`}>
+       
         </div>
     </div>
     )
