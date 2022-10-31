224cd358fb5cc7e94d57b06ee335826681c27366
Chandan2485 <chandan96kar@gmail.com>
2022-11-01 01:44:04 +0530

    amimation

---
+++ ballAnimation/src/Component/Animation.jsx
@@ -0,0 +1,11 @@
+import React,{useState} from "react";
+
+export function Animation (){
+
+    const [classname,setClassName]=useState()
+    return(
+        <div>chandan</div>
+    )
+        
+    
+}
